22d9ede685fc35e153f1ef30b322be580c0663c6
viniciusantana <viniciusantana@yahoo.com.br>
2023-01-11 17:22:21 -0300

    feat: aula 18 funcao print

---
+++ SEC03/aula2.py
@@ -0,0 +1,11 @@
+print(12) # 1 Argumento não nomeados
+print(1, 2) # 2 Argumentos não nomeados
+print(1, 2, sep='-') # 2 Argumentos e separador
+print(1, 2, 1111, sep='**') # 3 Argumentos e separador
+# \n \r - quebra de linha
+print(1, 2, 1111, sep='**', end='\n\n') # 3 Argumentos, separador e final de linha
+print(1, 2, 1111, sep='**', end='\n') # 3 Argumentos, separador e final-linha
+print(1, 2, 1111, sep='**', end='#') # 3 Argumentos, separador e final-linha
+print(1, 2, 1111, sep='**', end='#') # 3 Argumentos, separador e final-linha
+
+
