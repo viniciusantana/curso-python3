65342a74477b697621f5d615d114c2450a2e9cd8
Vinicius Santana <85767530+viniciusantana@users.noreply.github.com>
2023-01-14 23:21:08 -0300

    Revert "Seção 03"

--- SEC03/aula11.py
+++
@@ -1,9 +0,0 @@
-# Precedência entre os operadores aritméticos:
-
-# 1. (n + n)
-# 2. **
-# 3. * / // % 
-# 4. + -
-# Operadores de mesma precedência são executados da esquerda para direita
-conta_1 = (1 + int(0.5 + 0.5)) ** (5 + 5)
-print(conta_1)
